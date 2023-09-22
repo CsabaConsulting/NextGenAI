d5d8d33a0476e8e53dc12696c12aa5dc6ba8dbd8
MrCsabaToth <csaba.toth.us@gmail.com>
2023-09-22 15:20:06 -0700

    Very simple mock data for test

---
+++ first_mock_seed.sql
@@ -0,0 +1,27 @@
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description)
+VALUES ('1', '1', '1', '2023-09-19',
+"The image captures a breathtaking view of the iconic Eiffel Tower, located in the heart of Paris, France. The tower stands tall and proud, its imposing structure a testament to the engineering prowess of the era in which it was built. The Eiffel Tower is surrounded by lush green trees, adding a touch of serenity and natural beauty to the scene. In the foreground, a fountain with water jets creates a soothing and visually appealing focal point, further enhancing the overall aesthetic of the image. The water jets dance in the air, creating a mesmerizing display of movement and light. The sky above the Eiffel Tower is overcast with dark clouds, casting a dramatic and moody atmosphere over the scene. The clouds add depth and contrast to the image, emphasizing the tower's imposing presence and creating a sense of mystery and intrigue. The overall composition of the image is well-balanced, with the Eiffel Tower serving as the central focal point, surrounded by the lush green trees and the fountain with water jets. The dark clouds in the sky add a sense of drama and contrast, making the image both visually striking and emotionally evocative.");
+
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description)
+VALUES ('1', '2', '2', '2023-09-20',
+"The image depicts a stunning and grandiose architectural structure, possibly a palace or a government building, situated near a serene body of water at night. The building is adorned with intricate details, such as multiple towers and spires, which give it a regal and majestic appearance. The illumination of the building creates a warm and inviting ambiance, casting a soft glow on the surrounding water. The reflection of the building's lights on the water adds an extra layer of beauty to the scene, creating a sense of harmony between the structure and its natural surroundings. The calmness of the water further enhances the tranquility of the scene, providing a sense of peacefulness and serenity. The overall atmosphere of the image is one of grandeur, beauty, and tranquility, making it a captivating and visually appealing representation of a cityscape at night. The building in the image is likely the Hungarian Parliament Building, which is a famous landmark in Budapest, Hungary. The Parliament Building is a grandiose structure with multiple towers and spires, and it is illuminated at night, creating a beautiful and impressive sight on the banks of the Danube River.");
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Paris');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'France');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Eiffel Tower');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Overcast');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Clouds');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Trees');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Fountains');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('1', 'Dramatic');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Grandiose');
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Budapest');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Hungary');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Cityscape');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Nighttime');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Water');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Serene');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Peaceful');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Majestic');
+INSERT INTO `gdg-demos.images.tags` (image_id, tag) VALUES ('2', 'Grandiose');
