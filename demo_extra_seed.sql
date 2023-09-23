325a14124e5700f00897fb32e8fe1e4eaaa2c26b
MrCsabaToth <csaba.toth.us@gmail.com>
2023-09-23 12:14:26 -0700

    Extra seed for demo

---
+++ demo_extra_seed.sql
@@ -0,0 +1,12 @@
+-- Themes
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Landmarks', 1);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'People', 2);
+
+-- Eggs
+
+-- Restaurant
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'People', 2);
+
+-- Stonehedge
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Landmarks', 1);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'People', 2);
