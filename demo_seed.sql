984ba70f446d0fd4cef6a84cd38bb4e7eb70d0c7
MrCsabaToth <csaba.toth.us@gmail.com>
2023-09-23 11:19:01 -0700

    Leaving priority gaps in demo seed for presi fine tune

--- demo_seed.sql
+++ demo_seed.sql
@@ -3,55 +3,51 @@ VALUES ('1', '1', '1', '2022-09-19',
 "The image depicts a room with a unique and futuristic design. The room is filled with several large, white, egg-shaped objects arranged in a row, creating a striking visual display. The egg-shaped objects are positioned in a way that they appear to be floating in mid-air, giving the impression of a futuristic and unconventional space. The room is illuminated by multicolored lights, which are reflected off the walls and ceiling, creating a vibrant and visually striking atmosphere. The lights add an element of excitement and energy to the space, making it feel more dynamic and engaging. The combination of the egg-shaped objects and the multicolored lights creates a sense of wonder and curiosity, drawing the viewer's attention to the unique design of the room. The room's design appears to be inspired by a blend of modern and futuristic elements, with the egg-shaped objects and the multicolored lights contributing to the overall aesthetic. The use of white as the primary color for the egg-shaped objects adds a sense of cleanliness and simplicity to the space, while the multicolored lights add a touch of vibrancy and creativity. Overall, the image presents a visually striking and thought-provoking representation of a futuristic space, inviting the viewer to explore and appreciate its unique design.");
 
 INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Woman', 0);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'River Thames', 1);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Eiffel Tower', 2);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Clock Tower', 3);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Bridge', 4);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Clouds', 5);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Skyline', 6);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Coat', 7);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Hair', 8);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Clothing', 9);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Background', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'River Thames', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Eiffel Tower', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Clock Tower', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Bridge', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Clouds', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Skyline', 60);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Coat', 70);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Hair', 80);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Clothing', 90);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('1', 'Background', 100);
 
 INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description)
 VALUES ('1', '1', '2', '2022-09-19',
 "The image depicts a room with a unique and futuristic design. The room is filled with several large, white, egg-shaped objects arranged in a row, creating a striking visual display. The egg-shaped objects are positioned in a way that they appear to be floating in mid-air, giving the impression of a futuristic and unconventional space. The room is illuminated by multicolored lights, which are reflected off the walls and ceiling, creating a vibrant and visually striking atmosphere. The lights add an element of excitement and energy to the space, making it feel more dynamic and engaging. The combination of the egg-shaped objects and the multicolored lights creates a sense of wonder and curiosity, drawing the viewer's attention to the unique design of the room. The room's design appears to be inspired by a blend of modern and futuristic elements, with the egg-shaped objects and the multicolored lights contributing to the overall aesthetic. The use of white as the primary color for the egg-shaped objects adds a sense of cleanliness and simplicity to the space, while the multicolored lights add a touch of vibrancy and creativity. Overall, the image presents a visually striking and thought-provoking representation of a futuristic space, inviting the viewer to explore and appreciate its unique design.");
 
 INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'White eggs', 0);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Colorful Lights', 1);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Ceiling', 2);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Futuristic', 3);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Floor', 4);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Dim Lighting', 5);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Mysterious Atmosphere', 6);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Colorful Lights', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Ceiling', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Futuristic', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Floor', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Dim Lighting', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('2', 'Mysterious Atmosphere', 60);
 
 INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description)
 VALUES ('1', '1', '3', '2022-09-19',
 "The image depicts a cozy and inviting scene of four women enjoying a meal together at a restaurant. They are seated at a round table with white tablecloths, which adds a touch of elegance and sophistication to the setting. The women are smiling and posing for the camera, creating a warm and friendly atmosphere. The table is adorned with various items, such as glasses and bottles, which suggests that they are enjoying a refreshing beverage during their meal. The presence of these glasses and bottles adds to the overall ambiance of the image, creating a sense of relaxation and leisure. The women are dressed in stylish outfits, which adds to the overall aesthetic of the image. Their attire suggests that they are enjoying a special occasion or celebrating a milestone, further enhancing the warm and joyful atmosphere of the scene. Overall, the image conveys a sense of camaraderie, happiness, and relaxation, capturing the essence of a memorable dining experience shared among friends.");
 
 INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Woman', 0);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Table', 1);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Chairs', 2);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Bottles', 3);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Glasses', 4);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Tablecloths', 5);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Wall', 6);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Lighting', 7);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Background', 8);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Table', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Chairs', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Bottles', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Glasses', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Tablecloths', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Wall', 60);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Lighting', 70);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('3', 'Background', 80);
 
 INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description)
 VALUES ('1', '1', '4', '2022-09-19',
-"TThe image depicts a woman standing in front of the iconic Stonehenge, a prehistoric monument located in Wiltshire, England. The woman is dressed in a black jacket and a white dress, which adds a touch of elegance to the scene. She is holding a black umbrella, which serves as both a functional accessory and a stylish accent to her outfit.
-The woman is standing on a grassy area, surrounded by a fence that encloses the monument. This fence helps to maintain the historical and cultural significance of the site, while also providing a sense of protection and preservation for the ancient structure.
-Stonehenge itself is a remarkable feat of engineering and architecture, consisting of a series of massive stone pillars arranged in a circular pattern. The monument is believed to have been built over 5,000 years ago, and its purpose and meaning continue to be the subject of much debate and speculation among historians and archaeologists.
-The woman's pose, with her arm extended and pointing towards the monument, conveys a sense of awe and appreciation for the historical and cultural significance of Stonehenge. The image captures a moment of contemplation and reflection, as the woman takes in the grandeur and beauty of the ancient structure.
-Overall, the image offers a glimpse into the rich history and heritage of Stonehenge, while also showcasing the beauty and elegance of the woman's outfit and the surrounding landscape.");
+"The image depicts a woman standing in front of the iconic Stonehenge, a prehistoric monument located in Wiltshire, England. The woman is dressed in a black jacket and a white dress, which adds a touch of elegance to the scene. She is holding a black umbrella, which serves as both a functional accessory and a stylish accent to her outfit. The woman is standing on a grassy area, surrounded by a fence that encloses the monument. This fence helps to maintain the historical and cultural significance of the site, while also providing a sense of protection and preservation for the ancient structure. Stonehenge itself is a remarkable feat of engineering and architecture, consisting of a series of massive stone pillars arranged in a circular pattern. The monument is believed to have been built over 5,000 years ago, and its purpose and meaning continue to be the subject of much debate and speculation among historians and archaeologists. The woman's pose, with her arm extended and pointing towards the monument, conveys a sense of awe and appreciation for the historical and cultural significance of Stonehenge. The image captures a moment of contemplation and reflection, as the woman takes in the grandeur and beauty of the ancient structure. Overall, the image offers a glimpse into the rich history and heritage of Stonehenge, while also showcasing the beauty and elegance of the woman's outfit and the surrounding landscape.");
 
 INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Woman', 0);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Black Umbrella', 1);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'White dress', 2);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Stone Monument', 3);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Black Jacket', 4);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Fence', 5);
-INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Grassy Area', 6);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Black Umbrella', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'White dress', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Stone Monument', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Black Jacket', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Fence', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('4', 'Grassy Area', 60);
