e4b486729f071828675f5d811bf2e9341446f9aa
MrCsabaToth <csaba.toth.us@gmail.com>
2023-10-01 20:30:55 -0700

    Seeding 5 images from the hackathon (action/demo photo, Golden Gate, Fort Mason, Dock, Alactraz)

---
+++ additional_demo_seed.sql
@@ -0,0 +1,77 @@
+-- Hackathon in action group picture
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description, description2)
+VALUES ('1', '2', '5', '2023-09-23',
+"The image depicts a group of three individuals, two women and a man, seated at a long, rectangular table with a blue tablecloth. The table is surrounded by chairs, creating a comfortable and inviting atmosphere. The group is engaged in a collaborative activity, as they are actively working on a set of laptops placed in front of them. The two women are seated on either side of the man, who appears to be the leader or facilitator of the group. They are focused on their respective laptops, with their faces illuminated by the glow of the screens. The woman on the left is wearing a gray shirt, while the woman on the right is wearing a white shirt. The man is wearing a black shirt. The laptops are placed on a wooden surface, which adds a touch of warmth and texture to the image. The laptops themselves are sleek and modern, showcasing the latest technology and design. The group's interaction with the laptops suggests that they are involved in a creative or intellectual pursuit, such as brainstorming, problem-solving, or collaborative work. Overall, the image conveys a sense of collaboration, creativity, and productivity, as the group works together on their laptops. The blue tablecloth adds a touch of color and warmth to the scene, creating a welcoming and inviting atmosphere.",
+"The image shows a group of people sitting at a table with laptops in front of them. There is a blue tablecloth on the table, and the people are engaged in a discussion or working on their laptops.");
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Table', 0);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Blue Tablecloth', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Laptops', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Chairs', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Women', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Man', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Shirts', 60);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('5', 'Wooden surface', 70);
+
+-- Golden Gate
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description, description2)
+VALUES ('1', '2', '6', '2023-09-23',
+"The image depicts a serene and picturesque harbor scene, with several boats docked along the shore. The boats are arranged in a neat and organized manner, creating a visually appealing composition. The harbor is surrounded by a clear blue sky, which adds to the overall sense of tranquility and beauty in the scene. In the background, a bridge can be seen, which adds a sense of depth and structure to the image. The bridge serves as a connection between the harbor and the surrounding landscape, further enhancing the visual appeal of the scene. Overall, the image captures the essence of a peaceful and idyllic harbor setting, with the boats, sky, and bridge working together to create a harmonious and aesthetically pleasing scene.",
+"The image shows a harbor with several boats docked, a bridge in the background, and a clear blue sky.");
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('6', 'Boats', 0);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('6', 'Bridge', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('6', 'Sky', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('6', 'Water', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('6', 'Dock', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('6', 'Harbor', 50);
+
+-- Fort Mason historic building
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description, description2)
+VALUES ('1', '2', '7', '2023-09-23',
+"The image depicts a large, white building with multiple floors, located in a city or urban setting. The building has a prominent staircase leading up to the entrance, which adds a touch of elegance and sophistication to its architectural design. The building's facade is adorned with several windows, allowing natural light to illuminate the interior spaces.\nSurrounding the building is a spacious parking lot, where several cars are parked. The parking lot provides ample space for visitors and employees to park their vehicles, making it convenient for those who need to access the building.\nOn the side of the building, there is a fire escape, which serves as an essential safety feature in case of emergencies. The fire escape provides a means for people to quickly and safely evacuate the building in the event of a fire or other emergency situation.\nThe image is taken on a sunny day with a clear blue sky, which enhances the overall aesthetic appeal of the building. The sunlight casts a warm glow on the building's facade, highlighting its architectural details and creating a visually appealing composition.\nOverall, the image captures the essence of a well-designed, functional, and aesthetically pleasing building in a bustling urban environment.",
+"The image depicts a large, white building with multiple floors and a red fire escape ladder on the side. There are several cars parked in the parking lot in front of the building. The building is situated in a city setting with a clear blue sky above.");
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Building', 0);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Staircase', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Windows', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Parking lot', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Cars', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Fire escape', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Sky', 60);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Sunlight', 70);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Street', 80);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'People', 90);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Trees', 100);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Fence', 110);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('7', 'Lamp post', 120);
+
+-- Fort Mason Dock house
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description, description2)
+VALUES ('1', '2', '8', '2023-09-23',
+"The image depicts a picturesque scene of a large building situated near the water, with a dock and a fence surrounding it. The building appears to be made of brick and has multiple windows, giving it a classic and elegant appearance. The building's proximity to the water creates a sense of tranquility and serenity, while the presence of bicycles parked near the building adds a touch of liveliness to the scene. The clear and blue sky provides a beautiful backdrop for the image, further enhancing the overall aesthetic appeal of the scene.",
+"The image depicts a large building situated near the water, with a dock and a fence surrounding it. The building appears to be made of brick and has multiple windows. There are also several bicycles parked near the building. The sky is clear and blue, providing a serene backdrop for the scene.");
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Building', 0);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Dock', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Fence', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Bicycles', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Sky', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Water', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Windows', 60);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('8', 'Brick', 70);
+
+-- Alcatraz
+INSERT INTO `gdg-demos.images.images` (user_id, album_id, image_id, datetime, description, description2)
+VALUES ('1', '2', '9', '2023-09-23',
+"The image shows a tranquil scene with a vast expanse of blue water, stretching as far as the eye can see. In the distance, there is a small island with a lighthouse, standing tall and proud on the horizon. The sky above is a brilliant shade of blue, devoid of any clouds, giving the scene a sense of openness and freedom. The water itself appears to be calm and peaceful, reflecting the vibrant colors of the sky and the island. The overall atmosphere of the image is one of serenity and tranquility, inviting the viewer to bask in the beauty of the natural world.",
+"The image depicts a serene landscape with a large body of water, a small island with a lighthouse, and a clear blue sky with no clouds.");
+
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Water', 0);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Island', 10);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Lighthouse', 20);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Sky', 30);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Clouds', 40);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Horizon', 50);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Calmness', 60);
+INSERT INTO `gdg-demos.images.tags` (image_id, tag, priority) VALUES ('9', 'Tranquility', 70);
